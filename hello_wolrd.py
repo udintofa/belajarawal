e3344aae37b54cf317b68d0973b6e5268216e3ea
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-08 13:51:27 +0700

    hello_wolrd.py

--- hello_wolrd.py
+++ hello_wolrd.py
@@ -3,25 +3,12 @@ import pandas as pd
  
 st.write(
     """
-    # My first app
-    Hello, para calon praktisi data masa depan!
+    # Latihan web pertama Udin
+    Hallo Zahraku, ini aku lagi latihan buat web yaa, bisa minta tolong didukung bukan direwelin!
     """
 )
 
-st.write(pd.DataFrame({
-    'c1': [1, 2, 3, 4],
-    'c2': [10, 20, 30, 40],
-}))
-
-st.title('Belajar Analisis Data')
-st.header('Pengembangan Dashboard')
-st.subheader('Pengembangan Dashboard')
-st.caption('Copyright (c) 2023')
-code = """def hello():
-    print("Hello, Streamlit!")"""
-st.code(code, language='python')
-
-st.text('Halo, calon praktisi data masa depan.')
+st.text('Halo, bisa kerjakan soal berikut yaa....')
 
 st.latex(r"""
     \sum_{k=0}^{n-1} ar^k =
