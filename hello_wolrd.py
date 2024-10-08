992d39cd412792f66269bf965c2f18587c7a0d49
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-08 14:21:03 +0700

    hello_wolrd.py

--- hello_wolrd.py
+++ hello_wolrd.py
@@ -4,7 +4,7 @@ import numpy as np
  
 st.write(
     """
-    # Latihan web pertama Udin
-    Hallo Zahraku, ini aku lagi latihan buat web yaa, bisa minta tolong didukung bukan direwelin!
+    # Haloo Semuanya
+    Ini adalah web pertamaku
     """
 )
