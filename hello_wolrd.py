a0613fa457e142659398b41a47eb6825a156fd41
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-08 14:31:01 +0700

    hello_wolrd.py

--- hello_wolrd.py
+++ hello_wolrd.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
  
 st.write(
     """
@@ -8,3 +9,22 @@ st.write(
     Ini adalah web pertamaku
     """
 )
+
+st.write('''
+kita mempunyai sebuah fungsi yaitu:
+''')
+
+st.latex(r'y=x^2+2x+1')
+st.write('''
+Lalu, kita akan membuat grafik untuk fungsi tersebut
+''')
+
+def f(x):
+ return x**2+2*x+1
+
+x=np.linspace(0,10,100)
+
+plt.figure(figsize=(12, 5))
+plt.plot(x, f(x))
+plt.title("Plot Fungsi diatas")
+plt.show()
