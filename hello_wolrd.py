b9861a16f45519e153073fc5644d13c42aa781db
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-08 23:57:33 +0700

    Update hello_wolrd.py

--- hello_wolrd.py
+++ hello_wolrd.py
@@ -9,3 +9,24 @@ st.write(
     """
 )
 
+st.write('''
+kita mempunyai sebuah fungsi yaitu:
+''')
+
+
+st.latex(r'y=x^2+2x+1')
+st.write('''
+Lalu, kita akan membuat grafik untuk fungsi tersebut
+''')
+
+def f(x):
+ return x**2+2*x+1
+
+x=np.linspace(0,10,100)
+
+plt.figure(figsize=(12, 5))
+plt.plot(x, f(x))
+plt.title("Plot Fungsi diatas")
+plt.show()
+
+st.pyplot(plt)
