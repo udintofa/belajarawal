0c6404c577f15156135be92f6ffea19750e5036f
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-08 14:20:17 +0700

    hello_wolrd.py

--- hello_wolrd.py
+++ hello_wolrd.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import numpy as np
  
 st.write(
     """
@@ -7,15 +8,3 @@ st.write(
     Hallo Zahraku, ini aku lagi latihan buat web yaa, bisa minta tolong didukung bukan direwelin!
     """
 )
-
-st.write(
- """
- Kamu sayang aku gasi? kalau emang sayang ya didukung belajar terus.
- Aku gasuka kamu rewel terus yaa, tapi kadang juga suka kangen rewelan dari kamu si. Tapi ya gimana ya, jangan banyak-banyak rewelnya.
- Kalau kamu tetep rewel, nanti kamu aku suruh buat ngafalin rumus di bawah ini....!
- """)
-
-st.latex(r"""
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-""")
